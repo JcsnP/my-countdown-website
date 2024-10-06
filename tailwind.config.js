31aa3f66f0d2290e8362203269202f0506e2daed
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2024-10-07 00:02:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '600px',
+      }
+    }
+  },
+  plugins: [],
+}
